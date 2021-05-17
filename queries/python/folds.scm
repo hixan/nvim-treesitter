bb981aff6e9f1d0d7d993cd5b746a6e5fec6c649
Hixan <alex.88.elias@gmail.com>
2021-05-17 18:10:02 +1000

    make only paramatrised lines that have 'dict' as identifier folded.

--- queries/python/folds.scm
+++ queries/python/folds.scm
@@ -1,10 +1,11 @@
 (decorated_definition [(function_definition) (class_definition)]) @fold
 (
  [
-  (function_definition)
-  (class_definition)
   (import_from_statement)
   (import_statement)
+  (decorated_definition)
+  (function_definition)
+  (class_definition)
  ] [
   (function_definition)
   (class_definition)
@@ -17,10 +18,15 @@
 (with_statement (block)) @fold
 (try_statement (block)) @fold
 
+(
+(identifier) @ident
+(argument_list) @fold
+(#match? @ident "^dict$")
+)
+
 [
   (import_from_statement)
   (parameters)
-  (argument_list)
 
   (parenthesized_expression)
   (generator_expression)
